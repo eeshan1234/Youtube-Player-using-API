b201646a8e55aeabbc55503a60cc948945a1acee
eeshan1234 <98.eeshan@gmail.com>
2020-08-20 15:23:27 +0530

    Commit

---
+++ app/src/main/java/com/example/application/trial/youtubevides/Config.java
@@ -0,0 +1,14 @@
+package com.example.application.trial.youtubevides;
+
+public final class Config
+{
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCG25IwSEZcJuF5Te7kko9XawkHaEJ48Ws";
+
+
+}
+
+
